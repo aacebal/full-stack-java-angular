be69970812700552bae50319847ad575ff883941
Adel Acebal <adelacebal@yahoo.com>
2017-09-04 08:41:20 -0400

    angular class simulated

--- full-stack-app-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
+++ full-stack-app-angular-spring-boot/src/main/webapp/angular2/src/app/app.component.ts
@@ -10,12 +10,15 @@ export class AppComponent implements OnInit {
 
   public submitted: boolean;
   roomSearch: FormGroup;
+  rooms: Room[];
 
   ngOnInit() {
     this.roomSearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     })
+
+    this.rooms = ROOMS;
   }
 
   onSubmit({value, valid}: {value: RoomSearch, valid: boolean}) {
@@ -27,3 +30,31 @@ export interface RoomSearch {
   checkin: string;
   checkout: string;
 }
+
+export interface Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+  links: string
+}
+
+var ROOMS: Room[] = [
+  {
+    "id": "12345",
+    "roomNumber": "409",
+    "price": "20",
+    "links": ""
+  },
+  {
+    "id": "23456",
+    "roomNumber": "410",
+    "price": "25",
+    "links": ""
+  },
+  {
+    "id": "34567",
+    "roomNumber": "411",
+    "price": "30",
+    "links": ""
+  }
+]
